78d135adf5ea7c0311e4e251214ceef76d3ef038
laya <lavanyagunasekar@gmail.com>
2013-07-11 17:30:01 +0530

    made the base of system

--- src/computer_parts-activity/computer_parts.py
+++ src/computer_parts-activity/computer_parts.py
@@ -22,9 +22,11 @@ import gcompris
 import gcompris.utils
 import gcompris.skin
 import gcompris.bonus
+import gcompris.sound
 import goocanvas
 import pango
 import ConfigParser
+import random
 
 from gcompris import gcompris_gettext as _
 
@@ -49,12 +51,25 @@ class Gcompris_computer_parts:
     self.gcomprisBoard.maxlevel = 2
     self.gcomprisBoard.sublevel = 1
     self.gcomprisBoard.number_of_sublevel = 2
+    self.win = False
+    self.points = 0
 
     # Needed to get key_press
     gcomprisBoard.disable_im_context = True
 
+    # context is to specify INTERNAL or EXTERNAL parts
+    self.context = None
+    self.EXTERNAL = range(1, 7)
+    self.INTERNAL = range(7, 13)
+    
+    self.part_on_focus = None
+    self.REVEAL = True
+    self.HIDE = False
+
   def start(self):
     print "computer_parts start"
+
+    self.part = None
 
     # Set the buttons we want in the bar
     gcompris.bar_set(gcompris.BAR_LEVEL)
@@ -69,7 +84,7 @@ class Gcompris_computer_parts:
                                     self.gcomprisBoard.canvas.get_root_item())
 
     self.read_data()
-    self.display_explorer()
+    self.render_level()
 
   def end(self):
     print "computer_parts end"
@@ -102,9 +117,32 @@ class Gcompris_computer_parts:
   def pause(self, pause):
     print("computer_parts pause. %i" % pause)
 
-
+  # Called by gcompris when the user clicks on level icons
   def set_level(self, level):
     print("computer_parts set level. %i" % level)
+    self.gcomprisBoard.level = level
+    self.gcomprisBOard.sublevel = 1
+    self.next_level()
+
+  def next_level_click(self, event, target, item):
+    self.increment_level()
+
+  def increment_level(self):
+    self.gcomprisBoard.sublevel += 1
+
+    if (self.gcomprisBoard.sublevel > self.gcomprisBoard.number_of_sublevel):
+      self.gcomprisBoard.sublevel = 1
+      self.gcomprisBoard.level += 1
+      if (self.gcomprisBoard.level > self.gcomprisBoard.maxlevel):
+        self.gcomprisBoard.level = 1
+
+    self.render_level()
+
+  def increment_points(self):
+    self.points += 1
+    if self.points == 6:
+      self.win = True
+      self.increment_level()
 
 
   def read_data(self):
@@ -126,9 +164,26 @@ class Gcompris_computer_parts:
     self.dataset = config
     return True
 
+
   # Draws explorer screen items
-  def display_explorer(self):
+  def start_explorer(self):
+
+    if (self.gcomprisBoard.level == 1):
+      self.context = self.EXTERNAL
+    else:
+      self.context = self.INTERNAL
+
     gcompris.bar_location(660, -1, 0.5)
+
+    ok = goocanvas.Svg(parent = self.rootitem,
+                       svg_handle = gcompris.skin.svg_get(),
+                       svg_id = "#OK",
+                       tooltip = _("Next")
+                       )
+    ok.translate(150, -50)
+    ok.connect("button_press_event", self.next_level_click)
+    gcompris.utils.item_focus_init(ok, None)
+
 
     self.desc_tb = gtk.TextBuffer()
     self.desc_tv = gtk.TextView(self.desc_tb)
@@ -147,25 +202,86 @@ class Gcompris_computer_parts:
                                    )
     # self.desc_tv.show()
 
+    self.render_parts()
+
+
+  def render_parts(self):
+    print "rendering called"
+
     self.part_pos_x = 100
     self.part_pos_y = 100
 
-    for i in range(1,7):
+    if self.gcomprisBoard.sublevel == 2:
+      self.part_on_focus = self.HIDE
+    else:
+      self.part_on_focus = self.REVEAL
+
+    for i in self.context:
       name = str(self.dataset.get(str(i), "name"))
       desc = str(self.dataset.get(str(i), "desc"))
       img = str(self.dataset.get(str(i), "image"))
-      Part(self, name, desc, img, self.part_pos_x, self.part_pos_y)
+      Part(self, name, desc, img, self.part_pos_x, self.part_pos_y, self.part_on_focus)
 
       self.part_pos_x += 150
       if i == 3:
         self.part_pos_x = 100
         self.part_pos_y += 150
 
+
   def show_desc(self, desc):
     self.desc_tb.set_text(_(desc))
 
+
   def clear_desc(self):
     self.desc_tb.set_text(_("Mouse-over any part"))
+
+
+  def start_game(self):
+    print "THE GAME HAS STARTED"
+
+    self.parts_list = self.context[:]
+    random.shuffle(self.parts_list)
+ 
+    if self.gcomprisBoard.level == 1:
+      self.context = self.EXTERNAL
+    else:
+      self.context = self.INTERNAL
+
+    self.render_parts()
+    self.random_part()
+    
+    pass
+
+
+  def render_level(self):
+    if self.rootitem:
+      self.rootitem.remove()
+
+    self.rootitem = goocanvas.Group(parent = \
+      self.gcomprisBoard.canvas.get_root_item())  
+
+    if self.gcomprisBoard.sublevel == 1:
+      self.start_explorer()
+    else:
+      self.points = 0
+      self.start_game()
+
+  def check(self, name):
+    print "checking " + name 
+    if self.part == name:
+      self.increment_points()
+      gcompris.sound.play_ogg("sounds/tuxok.wav")
+      gcompris.bonus.display(gcompris.bonus.WIN, gcompris.bonus.TUX)
+      self.random_part()
+    else:
+      gcompris.bonus.display(gcompris.bonus.LOOSE, gcompris.bonus.TUX)
+
+  # Generate random part name.
+  def random_part(self):
+    print "points: " + str(self.points)
+    print "level: " + str(self.gcomprisBoard.level)
+    self.part = str(self.dataset.get(str(self.parts_list.pop()), "name"))
+    print "random set as: " + self.part
 
 
 class Part:
@@ -173,7 +289,7 @@ class Part:
   # desc is description about the part
   # img is the path to the image
   # x and y are position of the part
-  def __init__(self, computer_parts, name, desc, img, x, y):
+  def __init__(self, computer_parts, name, desc, img, x, y, on_focus):
     print "creating " + str(name)
     self.rootitem = computer_parts.rootitem
     self.image = gcompris.utils.load_pixmap(img)
@@ -196,8 +312,13 @@ class Part:
                                      width = self.width,
                                      height = self.height,
                                      )
-    self.part_item.connect("enter_notify_event", self.show_desc, self)
-    self.part_item.connect("leave_notify_event", self.clear_desc, self)
+
+    if on_focus:
+      self.part_item.connect("enter_notify_event", self.show_desc, self)
+      self.part_item.connect("leave_notify_event", self.clear_desc, self)
+    else:
+      self.part_item.connect("button_press_event", self.evaluate, self)
+
     gcompris.utils.item_focus_init(self.part_item, None)
 
   def show_desc(self, widget, target, event, item):
@@ -205,3 +326,6 @@ class Part:
 
   def clear_desc(self, widget, target, event, item):
     self.cp.clear_desc()
+
+  def evaluate(self, widget, target, event, item):
+    self.cp.check(self.name)
